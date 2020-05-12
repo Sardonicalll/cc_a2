c3f50331a9621e7075a9cb1b616fd92eeee2e163
De Yie Lu <s3700032@student.rmit.edu.au>
2020-05-12 22:35:29 +1000

    Created app.yaml, edited login.php
    app.yaml to be completed
    pushing login.php to test

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<title>Login</title>
+<body>
+<form action="#" method="get">
+Username: <input type="string" name="Username"><br>
+Password: <input type="string" name="Password"><br>
+</body>
+<?php
+
+?>
+</html>
