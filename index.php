32f6cc98189551bb0dc8604c757d279f29b2f258
De Yie Lu <s3700032@student.rmit.edu.au>
2020-05-16 19:44:24 +1000

    .

--- index.php
+++ index.php
@@ -1,5 +1,50 @@
 <!DOCTYPE html>
 <html>
+    <!-- Tab Title -->
+<title>Register</title>
+    <!-- Head containing stylesheet link -->
+<head>
+    <link rel="stylesheet" href="loginstyles.css">
+</head>
+    <!-- Header -->
+<header>
+    <h1>
+        Create an Account
+    </h1>
+</header>
+    <!-- Body -->
+<body>
+    <!-- Registration Form -->
+    <form method="post">
+        <input type="key" id="username" name="username" placeholder="Username">
+        <br>
+        <input type="key" id="email" name="email" placeholder="E-mail">
+        <br>
+        <input type="integer" id="password" name="password" placeholder="Password">
+        <br>
+        <input type="integer" id="confirmpassword" name="confirmpassword" placeholder="Confirm Password">
+        <br><br>
+        <input type="submit" value="Create Account">
+    </form>
+    <!-- Return to Login Button -->
+    <br>
+    Already have an account?
+    <br>
+    <input type="button" value="Return to Login Page" class="returnbtn" id="returnbtn" onClick="#"/>
+    <br><br>
+</body>
+    <!-- Footer -->
+<footer>
+    <p>
+        A Cloud Computing Assignment 2
+        <br>
+        Created by:
+        <br>
+        Caleb McCash | s3717184
+        <br>
+        De Yie Lu | s3700032
+    </p>
+</footer>
 <?php
 
 ?>
