6c6d41083baba5b82ea13928071dc8c02d562fa0
De Yie Lu <s3700032@student.rmit.edu.au>
2020-05-23 05:56:48 +1000

    Update register.php

--- register.php
+++ register.php
@@ -16,33 +16,15 @@
 <body>
     <!-- PHP -->
 <?php
-    // Email validation
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // Validate email format
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
-  }
-    //   Username validation
-  if (empty($_POST["username"])) {
-    $usernameErr = "Username is required";
-  }
-?>
 
+?>
     <!-- Registration Form -->
     <form method="post">
         <input type="key" id="username" name="username" placeholder="Username">
-        <span class="error"> <?php echo $usernameErr;?></span>
         <br>
         <input type="key" id="email" name="email" placeholder="E-mail">
-        <span class="error"> <?php echo $emailErr;?></span>
         <br>
         <input type="integer" id="password" name="password" placeholder="Password">
-        <br>
-        <input type="integer" id="confirmpassword" name="confirmpassword" placeholder="Confirm Password">
         <br><br>
         <input type="submit" value="Create Account">
     </form>
@@ -50,20 +32,12 @@
     <br>
     Already have an account?
     <br><br>
-    <a class="registerbtn" href="https://cloudcomp-a2.ts.r.appspot.com/">Return to Login Page</a>
+    <a class="loginbtn" href="https://cloudcomp-a2.ts.r.appspot.com/">Return to Login Page</a>
     <br><br>
 </body>
     <!-- Footer -->
 <footer>
-    <p>
-        A Cloud Computing Assignment 2
-        <br>
-        Created by:
-        <br>
-        Caleb McCash | s3717184
-        <br>
-        De Yie Lu | s3700032
-    </p>
+
 </footer>
 <!-- Stylesheet -->
 <style>
@@ -129,18 +103,6 @@ input[type=button] {
     text-decoration: none;
     display: inline-block;
     font-size: 18px;
-}
-
-footer {
-    background-color: #333;
-    padding: 14px 16px;
-    color: #FFFFFF;
-    text-align: center;
-    font-size: 12px;
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
 }
 </style>
 <?php
